da742fcf0198c3be4b1eac82a02c74262aebc7ce
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-07-31 19:00:55 +0200

    :sparkles: Implement bot with polyfact

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
-import fs from "fs";
-import * as t from "io-ts";
-import { generate } from "polyfact";
+import TelegramBot from "node-telegram-bot-api";
+import { generate, Chat, Memory, kv } from "polyfact";
+
+const language = "Japanese";
+const botName = "名前なし";
 
 (async () => {
-    const res = await generate("Count from 1 to 10");
+    const token = process.env.TELEGRAM_TOKEN || "";
 
-    console.log(res);
+    const bot = new TelegramBot(token, {polling: true});
+
+    bot.on('message', async (msg) => {
+      const chatId = msg.chat.id;
+
+      if (!(await kv.get(`telegram_language_bot:chatId:${chatId}`).catch(() => undefined))) {
+          const chat = new Chat({ systemPrompt: `You are a bot whose purpose is to help the user learn ${language}. Your name is ${botName}. The user's name is ${msg.chat.username}. Please refuse to answer to questions asked in any other language than ${language}.` });
+
+          await kv.set(`telegram_language_bot:chatId:${chatId}`, await chat.chatId);
+          const memory = new Memory();
+          await kv.set(`telegram_language_bot:memoryId:${chatId}`, await memory.memoryId);
+      }
+
+      const message = msg.text || "";
+
+      if (message === "/start") {
+          return;
+      }
+
+      const response = await generate(message, {
+          chatId: await kv.get(`telegram_language_bot:chatId:${chatId}`),
+          memoryId: await kv.get(`telegram_language_bot:memoryId:${chatId}`),
+      });
+
+      bot.sendMessage(chatId, response);
+    });
 })()
